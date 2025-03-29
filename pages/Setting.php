0bb107c4d65d43541287610cd0649a9cb4f0a29b
sandundimantha <sandundimantha58@gmail.com>
2025-03-30 05:19:57 +0530

    header and footer

---
+++ pages/Setting.php
@@ -0,0 +1,9 @@
+<?php
+include '../addphp/navbar.php';
+/*require_once 'config/db_config.php';*/
+?>
+
+<?php
+include '../addphp/footer.php';
+/*require_once 'config/db_config.php';*/
+?>
---
+++ pages/Users.php
@@ -0,0 +1,10 @@
+<?php
+include '../addphp/navbar.php';
+/*require_once 'config/db_config.php';*/
+?>
+
+
+<?php
+include '../addphp/footer.php';
+/*require_once 'config/db_config.php';*/
+?>
